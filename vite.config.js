1b451779857d8a5fe2a4f2a90d1fa211318a2c8f
ValentinDBS <pandarcha60@gmail.com>
2025-12-08 14:57:08 +0100

    Fix build error

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        dualWave1: resolve(__dirname, "dual-wave-1.html"),
-        dualWave2: resolve(__dirname, "dual-wave-2.html"),
-        dualWave3: resolve(__dirname, "dual-wave-3.html"),
-        dualWave4: resolve(__dirname, "dual-wave-4.html"),
-        dualWave5: resolve(__dirname, "dual-wave-5.html"),
       },
     },
   },
